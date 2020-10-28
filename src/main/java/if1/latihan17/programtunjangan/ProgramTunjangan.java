1f4e226fd42dbe2d225b8f4b846a138be4403c89
Rachman Aldiansyah <rachmanaldiansyah03@gmail.com>
2020-10-28 10:36:02 +0700

    Upload Project

--- src/main/java/if1/latihan17/programtunjangan/ProgramTunjangan.java
+++ src/main/java/if1/latihan17/programtunjangan/ProgramTunjangan.java
@@ -20,45 +20,41 @@ public class ProgramTunjangan {
         double tunjangan, gajiTotal;
         int gajiPokok;
         
-        // membuat scanner baru
-        Scanner scanner = new Scanner(System.in);
-        
         // tampilkan output ke layar
         System.out.println("=========Program Tunjangan=========");
         
         System.out.print("Berapa gaji pokok anda perbulan? : Rp.");
         // user memasukkan harga gajiPokok seorang pegawai
-        gajiPokok = scanner.nextInt();
+        Scanner keyboard_1 = new Scanner(System.in);
+        gajiPokok = keyboard_1.nextInt();
         
         System.out.print("Status Anda? (Menikah/Belum) :");
         // user memasukkan status seorang pegawai
-        status = scanner.nextLine();
+        Scanner keyboard_2 = new Scanner(System.in);
+        status = keyboard_2.nextLine();
         
         // operasi logika untuk menentukan status seorang pegawai
-        if(status == "Menikah") {
-            
-            tunjangan = (3.5 * gajiPokok);
-            gajiTotal = (gajiPokok * tunjangan);
-            // output hasil dari data yang dimasukkan user
-            System.out.println("=========Hasil Perhitungan Gaji Anda=========");
-            System.out.println("Gaji Pokok\t : " + gajiPokok);
-            System.out.println("Tunjangan\t  : " + tunjangan);
-            System.out.println("Total Gaji\t : " + gajiTotal);
-        
-        } else if(status == "Belum") {
-            
-            tunjangan = (0);
-            gajiTotal = (gajiPokok * tunjangan);
-            // output hasil dari data yang dimasukkan user
-            System.out.println("=========Hasil Perhitungan Gaji Anda=========");
-            System.out.println("Gaji Pokok\t : " + gajiPokok);
-            System.out.println("Tunjangan\t  : " + tunjangan);
-            System.out.println("Total Gaji\t : " + gajiTotal);
-        
-        } else {
-            
-            System.out.println("Silahkan Masukkan Data Yang Benar!");
-        
+        switch(status) {
+            case "Menikah" : 
+                tunjangan = (3.5 * gajiPokok);
+                gajiTotal = (gajiPokok * tunjangan);
+                // output hasil dari data yang dimasukkan user
+                System.out.println("=========Hasil Perhitungan Gaji Anda=========");
+                System.out.println("Gaji Pokok\t : " + gajiPokok);
+                System.out.println("Tunjangan\t  : " + tunjangan);
+                System.out.println("Total Gaji\t : " + gajiTotal);
+                break;
+            case "Belum" :
+                tunjangan = (0);
+                gajiTotal = (gajiPokok * tunjangan);
+                // output hasil dari data yang dimasukkan user
+                System.out.println("=========Hasil Perhitungan Gaji Anda=========");
+                System.out.println("Gaji Pokok\t : " + gajiPokok);
+                System.out.println("Tunjangan\t  : " + tunjangan);
+                System.out.println("Total Gaji\t : " + gajiTotal);
+                break;
+            default:
+                System.out.println("Isi Data Yang Benar!!");
         }
     }
 }
